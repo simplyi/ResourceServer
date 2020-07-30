e34e5c87e2a8260a4924048c296de5f96549bf2f
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-07-30 06:40:05 -0400

    Initial Commit

---
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.api.ResourceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/ws/api/ResourceServer/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.api.ResourceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
