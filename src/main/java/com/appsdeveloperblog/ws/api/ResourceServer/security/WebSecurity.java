663d151bab573d7dec112cd8edfba6f9b4f59dc4
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-07-31 07:56:23 -0400

    Created the WebSecurity class

---
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.ws.api.ResourceServer.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+}
