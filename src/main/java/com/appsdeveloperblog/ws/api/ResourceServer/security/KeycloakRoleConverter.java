de7a7df8c656557c414ffe74ac246607a0c2cb6e
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-01 15:03:36 -0400

    Created initial KeycloakRoleConverter class

---
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/KeycloakRoleConverter.java
@@ -0,0 +1,17 @@
+package com.appsdeveloperblog.ws.api.ResourceServer.security;
+
+import java.util.Collection;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+	@Override
+	public Collection<GrantedAuthority> convert(Jwt source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
