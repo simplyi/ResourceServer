de00da2d89162be405bdd3c263934e2bf29059af
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-05 10:57:27 -0400

    Enabled the @PreAuthorize annotation and added it to a Controller class

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,8 @@ public class UsersController {
 		return "Working...";
 	}
 	
-	@Secured("ROLE_developer")
+	@PreAuthorize("hasAuthority('ROLE_developer')") 
+	//@Secured("ROLE_developer")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id) {
         return "Deleted user with id " + id;
--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
-@EnableGlobalMethodSecurity(securedEnabled=true)
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
