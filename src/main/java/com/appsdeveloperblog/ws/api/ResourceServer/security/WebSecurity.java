d36f1a8a77816d806f3845c7e18d208ba8afb156
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-05 08:00:55 -0400

    Enabled the Global MethodSecurity and the @Secured annotation

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -1,11 +1,13 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.security;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@EnableGlobalMethodSecurity(securedEnabled=true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
