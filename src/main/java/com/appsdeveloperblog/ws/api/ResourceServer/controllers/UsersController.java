5c6d79486f2793b7aff3cad05404254adbd26902
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-05 09:13:17 -0400

    Added the @Secured example

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,9 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,12 @@ public class UsersController {
 	public String status() {
 		return "Working...";
 	}
+	
+	@Secured("ROLE_developer")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id) {
+        return "Deleted user with id " + id;
+    }
 	
 	
 }
