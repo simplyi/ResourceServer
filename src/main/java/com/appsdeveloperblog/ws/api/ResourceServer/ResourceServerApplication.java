0af36cb7278475fa145f34da2fd7b0d7bf8dce63
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-19 06:32:20 -0400

    Configured resource server to be Eureka Discovery Client

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/ResourceServerApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/ResourceServerApplication.java
@@ -2,7 +2,9 @@ package com.appsdeveloperblog.ws.api.ResourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceServerApplication {
 
