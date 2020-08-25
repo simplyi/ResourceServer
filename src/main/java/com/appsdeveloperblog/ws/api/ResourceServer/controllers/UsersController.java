ba32efc1a71804828c360b555dc2404fd0a2e6eb
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-25 11:07:20 -0400

    Returning the running port number from Resource Server

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,10 +18,13 @@ import com.appsdeveloperblog.ws.api.ResourceServer.response.UserRest;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+	
+	@Autowired
+	Environment env;
 
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working...";
+		return "Working on port: " + env.getProperty("local.server.port");
 	}
 	
 	@PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject") 
