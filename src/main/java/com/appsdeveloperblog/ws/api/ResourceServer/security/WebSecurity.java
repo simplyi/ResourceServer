459877d108d0b1a3fdadec899eaec0f758f9f1a9
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-06-06 16:29:14 -0400

    Resource Server update to Spring Boot version 3.1.0

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -3,51 +3,51 @@ package com.appsdeveloperblog.ws.api.ResourceServer.security;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
+@Configuration
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
+public class WebSecurity {
+
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-	 
-		http.cors().and()
-			.authorizeRequests()
-					.antMatchers(HttpMethod.GET, "/users/status/check") 
-					//.hasAuthority("SCOPE_profile")
-					.hasRole("developer")
-					//.hasAnyAuthority("ROLE_developer")
-					//.hasAnyRole("devleoper","user")
-				.anyRequest().authenticated()
-				.and()
-			.oauth2ResourceServer()
-			.jwt()
-			.jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+				.authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.GET, "/users/status/check")
+						// .hasAuthority("SCOPE_profile")
+						.hasRole("developer")
+						// .hasAnyAuthority("ROLE_developer")
+						// .hasAnyRole("devleoper","user")
+						.anyRequest().authenticated())
+				.oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+		return http.build();
 	}
-	
+ 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", corsConfiguration);
-		
+
 		return source;
 	}
-	
+
 }
